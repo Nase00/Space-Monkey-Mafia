ff9940bc9a44ea9284544f16d2591c412dd6f40e
Sean <nase00@gmail.com>
2015-01-06 16:11:31 -0600

    refactored controller

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -13,17 +13,10 @@ end
 # altcode 4a4ad4e690121617b837a13e60e36736
 
 get '/users/:email_address/email' do
-  uri = URI('http://dbc-mail.herokuapp.com/api/' + params[:email_address] + '/messages?api_token=dd4cf85cd0ee9ffb448340bc66507619')
-  response = Net::HTTP.get_response(uri)
-  case response.code
-  when '200'
-    inbox = Hash.from_xml(response.body)
-    parse_into_objects(inbox["messages"]).to_json
-  when '404'
-    "email address does not exist"
-  when '429'
-    "too many requests, please try again later"
-  when '502'
-    "planned failure"
+  user = User.find_or_create_by(email_address: params[:email_address]) do |user|
+    user.id = 93
+    user.name = "Super Monkey Mafia"
+    user.password = "hunter2"
   end
+  get_emails(user)
 end
--- app/helpers/parser.rb
+++ app/helpers/parser.rb
@@ -1,4 +1,33 @@
+TOKEN = "dd4cf85cd0ee9ffb448340bc66507619"
+
 helpers do
+  def get_emails(user)
+    query = "?api_token="
+    uri = URI('http://dbc-mail.herokuapp.com/api/' + user.email_address + '/messages' + query + TOKEN)
+    response = Net::HTTP.get_response(uri)
+    case response.code
+    when '200'
+      # count_emails(user)
+      inbox = Hash.from_xml(response.body)
+      parse_into_objects(inbox["messages"])
+      emails = Email.where(receiver_id: user.id).to_json
+
+    when '404'
+      "email address does not exist"
+    when '429'
+      "too many requests, please try again later"
+    when '502'
+      "planned failure"
+    end
+  end
+
+  # def count_emails(user)
+  #   token = "?api_token="
+  #   uri = URI('http://dbc-mail.herokuapp.com/api/' + user.email_address + '/messages' + token)
+  #   response = Net::HTTP.get_response(uri)
+  #   # http://dbc-mail.herokuapp.com/api/supermonkey@mafia.com/messages/count?last_id=10&api_token=dd4cf85cd0ee9ffb448340bc66507619
+  # end
+
   def parse_into_objects(xml_string)
     xml_string.each do |hash|
       receiver = User.find_or_create({id: hash["email_address_id"]})
