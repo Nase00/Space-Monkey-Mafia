12e49fc1ba002b4ba28e928269aae69ae3d5d8ca
Sean <nase00@gmail.com>
2015-01-06 15:22:15 -0600

    lol

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-150.times do
-  # password = "test"
-  # User.create(name: Faker::Name.name, email_address: Faker::Internet.email, password: password)
-end
+# 150.times do
+#   password = "test"
+#   User.create(name: Faker::Name.name, email_address: Faker::Internet.email, password: password)
+# end
