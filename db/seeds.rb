bd065e0e5a22f9d5bce1579bbcb74acfcf0014b4
Sean <nase00@gmail.com>
2015-01-06 16:11:44 -0600

    removed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,7 @@
 #   password = "test"
 #   User.create(name: Faker::Name.name, email_address: Faker::Internet.email, password: password)
 # end
+
+# 200.times do
+#   sender_id = (51..100).rand
+#   Email.create!()
