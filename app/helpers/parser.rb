3a00ff9cd0a242f555858605a666aff323decd91
Sean <nase00@gmail.com>
2015-01-06 17:45:20 -0600

    parse inbox with num as additional argument

--- app/helpers/parser.rb
+++ app/helpers/parser.rb
@@ -9,9 +9,10 @@ helpers do
     response = Net::HTTP.get_response(uri)
     case response.code
     when '200'
-      if count_emails(user).to_i > 0
+      # return new_emails(user)
+      if new_emails(user) > 0 || initial_fetch?(user)
         inbox = Hash.from_xml(response.body)
-        parse_into_objects(inbox["messages"])
+        parse_into_objects(inbox["messages"], new_emails(user))
       end
       emails = Email.where(receiver_id: user.id).to_json
     when '404'
@@ -23,19 +24,28 @@ helpers do
     end
   end
 
-  def count_emails(user)
-    query = "count?last_id=" + Email.most_recent_id(user).to_s + "&api_token="
+  def initial_fetch?(user)
+    return true if user.received_emails.empty?
+  end
+
+  def new_emails(user)
+    query = "count?last_id=" + user.most_recent_received_email_id.to_s + "&api_token="
     uri = URI('http://dbc-mail.herokuapp.com/api/' + user.email_address + '/messages/' + query + TOKEN)
     response = Net::HTTP.get_response(uri)
-    response.body
+    hash = Hash.from_xml(response.body)
+
+    return hash["hash"]["count"].to_i rescue 50
+
     # http://dbc-mail.herokuapp.com/api/supermonkey@mafia.com/messages/count?last_id=10&api_token=dd4cf85cd0ee9ffb448340bc66507619
   end
 
-  def parse_into_objects(xml_string)
-    xml_string.each do |hash|
-      receiver = User.find_or_create({id: hash["email_address_id"]})
-      sender = User.find_or_create({email_address: hash["from"]})
-      email = Email.create!(id: hash["id"], subject: hash["subject"], receiver: receiver, sender: sender, body: hash["body"])
+  def parse_into_objects(xml_string, num)
+    xml_string.each_with_index do |hash, i|
+      if i < num
+        receiver = User.find_or_create({id: hash["email_address_id"]})
+        sender = User.find_or_create({email_address: hash["from"]})
+        email = Email.create!(id: hash["id"], subject: hash["subject"], receiver: receiver, sender: sender, body: hash["body"])
+      end
     end
   end
 end
