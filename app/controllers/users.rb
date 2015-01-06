76ae651de8069f5c4ddffcc051bceeeffc4af72c
Sean <nase00@gmail.com>
2015-01-06 15:23:05 -0600

    removed old comments from route

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -15,14 +15,8 @@ end
 get '/users/:email_address/email' do
   uri = URI('http://dbc-mail.herokuapp.com/api/' + params[:email_address] + '/messages?api_token=dd4cf85cd0ee9ffb448340bc66507619')
   response = Net::HTTP.get_response(uri)
-  # xml_string = Net::HTTP.new(uri.host, uri.port)
-  # code = (Net::HTTP.get_response(uri)).code
   case response.code
   when '200'
-    # return xml_string.to_json
-    # a = []
-    # request.methods.each { |i| a << i.to_s }
-    # return a.join(' ')
     inbox = Hash.from_xml(response.body)
     parse_into_objects(inbox["messages"]).to_json
   when '404'
@@ -31,7 +25,5 @@ get '/users/:email_address/email' do
     "too many requests, please try again later"
   when '502'
     "planned failure"
-  # else
-  #   "wtf I dunno"
   end
 end
