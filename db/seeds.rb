2705e10dc76d9890b7c974988aa031bd77d7045c
Apprentice <nase00@gmail.com>
2015-01-07 09:13:03 -0600

    Seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,3 @@
-# 150.times do
-#   password = "test"
-#   User.create(name: Faker::Name.name, email_address: Faker::Internet.email, password: password)
-# end
-
-# 200.times do
-#   sender_id = (51..100).rand
-#   Email.create!()
+# be rake db:drop && be rake db:create && be rake db:migrate && be rake db:seed
+User.create(id: 93, email_address: "supermonkey@mafia.com", name: "Super Moneky Mafia", password: "hunter2")
+User.create(id: 92, email_address: "squash@buddies.com", name: "Squash Buddies", password: "hunter2")
